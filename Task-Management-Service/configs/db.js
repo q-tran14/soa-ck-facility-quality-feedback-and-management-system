9f9009213a41c71e92907566088b1ac4d4c9f68d
ngwine <quyntn.longan@gmail.com>
2025-12-09 02:26:51 +0700

    Update 4 + file .pem

--- Task-Management-Service/configs/db.js
+++ Task-Management-Service/configs/db.js
@@ -1,13 +1,28 @@
-// Task-Management-Service/configs/db.js
 const mongoose = require("mongoose");
+const fs = require("fs");
+const path = require("path");
 
 const connectDB = async () => {
   try {
-    if (!process.env.MONGO_URI) {
+    const uri = process.env.MONGO_URI;
+    if (!uri) {
       throw new Error("MONGO_URI is not defined in .env");
     }
 
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const certPath = process.env.MONGO_X509_KEY_PATH;
+    if (!certPath) {
+      throw new Error("MONGO_X509_KEY_PATH is not defined in .env");
+    }
+
+    const absPath = path.resolve(certPath);
+    if (!fs.existsSync(absPath)) {
+      throw new Error(`X.509 cert file not found at: ${absPath}`);
+    }
+
+    const conn = await mongoose.connect(uri, {
+      tls: true,
+      tlsCertificateKeyFile: absPath,
+    });
 
     console.log(
       "======= Task-Management-Service: MongoDB connected =======",
